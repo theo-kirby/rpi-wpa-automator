53e41b7a980295b9912c532f35ff5934eadb2263
theo-kirby <theokirby15@gmail.com>
2021-06-20 14:30:37 -0400

    country code flag

--- wpa_helper.py
+++ wpa_helper.py
@@ -1,15 +1,17 @@
 import os, getpass
 import sys, getopt
 
+cc = ''
 ssid = ''
 pswd = ''
+usage = '\n Usage : wpa_helper.py -n <Network Name> -p <Network Password> -c <Country Code>\n'
     
-def mkFiles(s,p):
+def mkFiles(s,p,c):
         
     os.system('touch wpa_supplicant.conf ssh')
     
     wpa = '''
-country=US
+country='''+c+'''
 ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 update_config=1
 
@@ -31,6 +33,7 @@ def getParams():
 
     global ssid
     global pswd
+    global cc
 
     #ssid = input("\n SSID : ")               #  Wifi Network Name
     #pswd = getpass.getpass("\n PSWD : ")     #  Wifi Network Password
@@ -38,18 +41,20 @@ def getParams():
   
   
 def main(argv):
-       
+    
    try:
-      opts, args = getopt.getopt(argv,"n:p:",["nname=","npass="])
+      opts, args = getopt.getopt(argv,"n:p:c:",["nname=","npass=", "ccode="])
       
    except getopt.GetoptError:
-      print('wpa_helper.py -n <Network Name> -p <Network Password>')
+      print(usage)
       sys.exit(2)
       
+   options = len(opts)
+   
    for opt, arg in opts:
        
       if opt == '-h':
-         print('wpa_helper.py -n <Network Name> -p <Network Password>')
+         print(usage)
          sys.exit()
          
       elif opt in ("-n", "--Network Name"):
@@ -57,14 +62,22 @@ def main(argv):
           ssid = arg
          
       elif opt in ("-p", "--Network Password"):
-        global pswd
-        pswd = arg
-         
+          global pswd
+          pswd = arg
+          
+      elif opt in ("-c", "--Country Code"):
+          global cc
+          cc = arg
+          
+   if options == 0:
+      print(usage)
+
+  
 if __name__ == "__main__":
     
     main(sys.argv[1:])
     getParams()
-    mkFiles(ssid, pswd)
+    mkFiles(ssid, pswd, cc)
     
-#os.system('cp wpa_supplicant.conf ssh /Volumes/boot')
+    #os.system('cp wpa_supplicant.conf ssh /Volumes/boot')
 
